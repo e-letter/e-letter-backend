191954f61eb5445ba72bade4a8a23512807eedfb
Jars44 <maulidfajar163@gmail.com>
2026-06-07 18:00:57 +0700

    refactor(repository): remove user repository
    Delete UserRepository interface and its implementation to clean up unused repository files.

--- internal/repository/user_repository.go
+++
@@ -1,8 +0,0 @@
-package repository
-
-import "github.com/Refliqx/backend-eletter/internal/domain"
-
-type UserRepository interface {
-	Create(user domain.User) error
-	IsEmailExists(email string) bool
-}
